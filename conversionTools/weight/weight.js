9d8837d25c69084e90646b1ab48860a6bbf60ef8
Prakhar Jain <61567266+prakhar-ai@users.noreply.github.com>
2020-10-06 02:27:22 +0530

    added weight conversion tool

---
+++ conversionTools/weight/weight.js
@@ -0,0 +1,11 @@
+
+    function kiloweightConvert(value) { 
+        document.getElementById("Pounds").innerHTML = 
+          value * 2.2046; 
+        document.getElementById("Ounces").innerHTML = 
+          value * 35.274; 
+        document.getElementById("Grams").innerHTML = 
+          value * 1000; 
+        document.getElementById("Stones").innerHTML = 
+          value * 0.1574; 
+    } 
