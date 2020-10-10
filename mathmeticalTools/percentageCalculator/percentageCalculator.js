3654447bd0bbb125a301b0a93c75e174fc7a440f
adata111 <ahana.datta.ravenclaw@gmail.com>
2020-10-11 02:35:32 +0530

    Improved design

--- mathmeticalTools/percentageCalculator/percentageCalculator.js
+++ mathmeticalTools/percentageCalculator/percentageCalculator.js
@@ -1,7 +1,25 @@
+function op(){
+var coll = document.getElementById("collapsible");
+var i;
+console.log("hi");
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    console.log("hi");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}}
+
 function findPart(){
 	var tot = document.getElementById("tot1").value;
 	var perc = document.getElementById("perc1").value;
 	var ans = (tot * perc) / 100;
+	document.getElementById("error1").innerHTML = "";
 	document.getElementById("ans1").innerHTML = ans;
 }
 
@@ -9,7 +27,11 @@ function findPerc(){
 	var tot = document.getElementById("tot2").value;
 	var part = document.getElementById("part2").value;
 	var ans = (part / tot) * 100;
+	document.getElementById("error2").innerHTML = "";
 	document.getElementById("ans2").innerHTML = ans+'%';
+	if(tot==0){
+		document.getElementById("error2").innerHTML = "You tried to divide by 0. Check out the formula!";
+	}
 }
 
 function findTot(){
@@ -20,7 +42,10 @@ function findTot(){
 		ans=0;
 	else 
 		ans = (part * 100) / perc;
+	document.getElementById("error3").innerHTML = "";
 	document.getElementById("ans3").innerHTML = ans;
+	if(perc == 0 && part!=0)
+		document.getElementById("error3").innerHTML = "You tried to divide by 0. Check out the formula!";
 }
 
 function findPercChange(){
@@ -33,7 +58,7 @@ function findPercChange(){
 	}
 	else
 		ans = (100 * diff) / ini;
-	
+	document.getElementById("error4").innerHTML = "";
 	if(diff<0){
 		ans = 0-ans;
 		document.getElementById("ans4").innerHTML = ans +'% decrease';
@@ -44,5 +69,8 @@ function findPercChange(){
 	}
 	else{
 		document.getElementById("ans4").innerHTML = ans +'% increase';
+	}
+	if(ini==0 && fin!=0){
+		document.getElementById("error4").innerHTML = "You tried to divide by 0. Check out the formula!";
 	}
 }
