8eb6199a8ad509b895be993095f71541a47e9b65
adata111 <ahana.datta.ravenclaw@gmail.com>
2020-10-11 03:52:14 +0530

    percentage calculator done

--- mathmeticalTools/percentageCalculator/percentageCalculator.js
+++ mathmeticalTools/percentageCalculator/percentageCalculator.js
@@ -24,7 +24,6 @@ function findPart(){
 	var tot = document.getElementById("tot1").value;
 	var perc = document.getElementById("perc1").value;
 	var ans = (tot * perc) / 100;
-	document.getElementById("error1").innerHTML = "";
 	document.getElementById("ans1").innerHTML = ans;
 }
 
