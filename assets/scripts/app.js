60f6bac64f0e7361ba75751aaba6c7ce2d267b82
Negar <n.jamalifard@gmail.com>
2020-10-20 17:22:23 +0200

    [Feature] generate navbar dynamically (#118)
    * create a variable to store categories
    
    * create a variable to store tools
    
    * register variables globaly in footer
    
    * create a template class to generate navbar items dynamiclly
    
    * initiate navbar template in the general js file
    
    * update README and CONTRIBUTING files
    
    * provide a template example for tool's html file with all the necessary scripts and elements
    
    * rename example file to boilerplate
    
    * Update README and CONTRIBUTING files
    
    * Revert "Color Converter Added (#119)"
    
    This reverts commit fe26108f6046e80d8a4e2f0dbbaf896c812f65d9.

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+/*
+* General App Scrips
+* Notice: Only general scripts should be placed here
+* Created by Negar Jamalifard
+* */
+
+(function (){
+    const template = new Templates();
+    template.appendNavItems();
+})()
